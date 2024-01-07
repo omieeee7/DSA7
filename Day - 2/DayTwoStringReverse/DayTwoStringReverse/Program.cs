37d0a274127c07de452c0a4bb72555b57f53d530
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-07 23:25:38 +0530

    Add files via upload

---
+++ Day - 2/DayTwoStringReverse/DayTwoStringReverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwoStringReverse
+{
+    internal class Program
+    {
+       public string ReverseTheString()
+        {
+            string Data = "omkar";
+            string Output = "";
+            for(int i=Data.Length-1; i>=0; i--)
+            {
+                Output+= Data[i];
+            }
+            return Output;
+        }
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.ReverseTheString());
+            Console.ReadLine();
+        }
+    }
+}
