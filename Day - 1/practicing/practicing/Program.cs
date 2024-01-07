da2a9998a74564f917b81296a0ab180958e68ecf
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-07 23:28:42 +0530

    Add files via upload
    here we day 1st task where we removing duplicate char from

---
+++ Day - 1/practicing/practicing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicing
+{
+    internal class Program
+    {
+        public string DuplicateCharInString(string Data)
+        {
+            string garbage = "";
+            string Output = "";
+            foreach (char c in Data)
+            {
+                if(garbage.IndexOf(c) == -1)
+                {
+                    garbage += c;
+                    Output += c;
+                }
+            }
+            return garbage;
+        }
+        static void Main(string[] args)
+        {
+            Program p =new Program();
+            Console.WriteLine(p.DuplicateCharInString("Omkkkarr"));
+            Console.ReadLine();
+        }
+    }
+}
