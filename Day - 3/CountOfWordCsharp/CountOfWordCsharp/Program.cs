c7cb86a1897be6e52eea51a2f2c5cf2b3a99b046
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-08 22:58:01 +0530

    Add files via upload
    here we completed count of char with spacing tomorrow we count char without space

---
+++ Day - 3/CountOfWordCsharp/CountOfWordCsharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOfWordCsharp
+{
+    class Program
+    {
+       public void CharCountWithSpace()
+        {
+
+            string str = "omkar";
+            int tem = 0;
+            tem = str.Count();
+            {
+                Console.WriteLine("count=" + tem);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.CharCountWithSpace();
+            Console.ReadLine();
+        }
+    }
+}
