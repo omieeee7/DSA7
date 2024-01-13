468d51ccc9c77b64db8dc20163c859bd05d9d62c
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-13 09:41:10 +0530

    Add files via upload
    Here we learned how do reverse int by mode and division

---
+++ Day - 6/ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        void ReverseNumbers()
+        {
+            Console.WriteLine("Enter Your Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int rem;
+            Console.Write(" Your Reverse number is ");
+            while (num > 0)
+            {
+                rem = num % 10;
+                Console.Write(rem);
+                num = num / 10;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.ReverseNumbers();
+            Console.ReadLine();
+        }
+    }
+}
