5d13bb2e2d22ad0e15e8375696d7937bb7534605
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-15 21:40:07 +0530

    Add files via upload
    Today we completed task of number is Prime or Not

---
+++ Day - 8/PrimeNoOrNot/PrimeNoOrNot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNoOrNot
+{
+    internal class Program
+    {
+        public void Prime()
+        {
+            Console.WriteLine("Enter your No :- ");
+            int input =Convert.ToInt32(Console.ReadLine());
+            while (input != 0)
+            {
+                if (input % 2 == 0)
+                {
+                    Console.WriteLine("This is Prime Number");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("This is Not Prime Number");
+                    break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Prime();
+            Console.ReadLine();
+        }
+    }
+}
