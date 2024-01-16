35514ed8a7685d166b1da1bf52d48724a012ecff
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-16 21:22:34 +0530

    Add files via upload
    today we learn star pattern in in single for loop having two two loops

---
+++ Day - 9/StarPattern/Pyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramid
+{
+    internal class Program
+    {
+
+        public void StarTriangle()
+        {
+            for(int i = 1; i <= 5; i++)
+            {
+                for(int j=5;j>=i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int k=1;k<=i; k++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.StarTriangle();
+            Console.ReadLine();
+        }
+    }
+}
