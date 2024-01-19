0632321a106097383b98c4ed80cdf210c22ac40e
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-19 22:44:04 +0530

    Add files via upload
    here we done occurance of each charachter in string separatly

---
+++ Day -12/OccuranceOfEachCharacterInString/OccuranceOfEachCharacterInString/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OccuranceOfEachCharacterInString
+{
+    internal class Program
+    {
+        public void Countcharacter()
+        {
+            Console.WriteLine("Enter Your String :- ");
+            string input=Convert.ToString(Console.ReadLine());
+
+            Dictionary<char, int> cCount = new Dictionary<char, int>();
+
+            foreach (var c in input)
+            {
+                if (c != ' ')
+                {
+                    if (!cCount.ContainsKey(c))
+                    {
+                        cCount.Add(c, 1);
+                    }
+                    else
+                    {
+                        cCount[c]++;
+                    }
+                }
+
+            }
+            foreach (var character in cCount)
+            {
+                Console.WriteLine("{0} - {1}", character.Key, character.Value);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p =new Program();
+            p.Countcharacter();
+            Console.ReadLine();
+        }
+    }
+}
