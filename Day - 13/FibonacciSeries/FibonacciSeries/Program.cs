dd9297f90dd45b5e9e4a684d10e8630b131629f9
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-21 00:01:25 +0530

    Add files via upload
    Here is the simple fibonacci hard coded code were fibonacci is series of number starst from 0 and 1 . Next Number will be the sum of last two numbers

---
+++ Day - 13/FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    internal class Program
+    {
+        //Fibonacci number :- it is series of number starts from 0 and 1. Next number will be the sum of last two numbers
+        public void Fibonacci()
+        {
+            int number1 = 0;
+            int number2 = 1;
+            int sum;
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+       
+                for (int i = 0; i < 10; i++)
+                {
+                    sum = number1 + number2;
+                    Console.WriteLine(sum);
+                    number1 = number2;
+                    number2 = sum;
+
+                }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();  
+            p.Fibonacci();
+            Console.ReadLine();
+        }
+    }
+}
