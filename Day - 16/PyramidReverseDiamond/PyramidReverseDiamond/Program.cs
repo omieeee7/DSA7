56411be8970a0717a09e6b51224fefc049d541d4
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-23 22:44:48 +0530

    Add files via upload
    making of diamond with pyramid

---
+++ Day - 16/PyramidReverseDiamond/PyramidReverseDiamond/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyramidReverseDiamond
+{
+    internal class Program
+    {
+        void diamond()
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 4; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (j = 2; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 4; j >= i; j = (j - 1))
+                {
+                    Console.Write("*");
+                }
+                for (j = i; j <= 3; j = j + 1)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.diamond();
+            Console.ReadLine();
+        }
+    }
+}
