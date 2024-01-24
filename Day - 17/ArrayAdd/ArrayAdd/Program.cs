380354099390dcbccdbb81fc17557733ca4efb37
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-24 23:00:44 +0530

    Add files via upload
    Array addition and square pattern design

---
+++ Day - 17/ArrayAdd/ArrayAdd/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAdd
+{
+    internal class Program
+    {
+        public void ArraSquare()
+        {
+            Console.WriteLine("Enter Numbers to create Square :- ");
+            int[,] a = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Your Array Output Square :- ");
+            for (int i = 0;i < 3; i++)
+            {
+                for(int j = 0;j < 3; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+           p.ArraSquare();
+            Console.ReadLine();
+        }
+    }
+}
