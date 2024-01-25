067d998b7d26e19214a9fa764659dbceac185e02
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-25 23:17:23 +0530

    Add files via upload
    Here we learn how to do addition in array

---
+++ Day - 18/DualArrayAdd/DualArrayAdd/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DualArrayAdd
+{
+    internal class Program
+    {
+        void arrayadd()
+        {
+            int[,] A = new int[3, 3];
+            int[,] B = new int[3, 3];
+            int[,] C = new int[3, 3];
+            int i, j;
+            Console.WriteLine("Enter Your 1St array values :-");
+            for ( i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    A[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Enter your 2nd Array :- ");
+            for(i = 0; i < 3; i++)
+            {
+                for(j = 0;j < 3; j++)
+                {
+                    B[i,j]=Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for(i = 0;i < 3; i++)
+            {
+                for( j = 0;j < 3; j++)
+                {
+                    C[i, j] = A[i, j] + B[i,j];
+                } 
+            }
+
+            Console.WriteLine("Your Array Addition = ");
+            for(i = 0; i < 3; i++)
+            {
+                for(j=0;j < 3; j++)
+                {
+                    Console.Write(C[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+static void Main(string[] args)
+        {
+
+            Program p = new Program();
+            p.arrayadd();
+            Console.ReadLine();
+        }
+    }
+}
