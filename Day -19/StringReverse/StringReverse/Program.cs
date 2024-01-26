5c2ea8cdd5dc1e331860d257c144ef6a65739bae
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-26 23:21:23 +0530

    Add files via upload
    here we started to do same machine task to more code undestanding

---
+++ Day -19/StringReverse/StringReverse/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    internal class Program
+    {
+        public void StringReverse()
+        {
+            string input;
+            string output="";
+            Console.WriteLine("Enter Your String :-");
+            input = Convert.ToString(Console.ReadLine());
+            string temp = input;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+            Console.WriteLine("Your Output is = "+output);
+            Console.WriteLine(output +" is Output and Input is "+temp);
+            if(temp == output)
+            {
+                Console.WriteLine("String is palendrome");
+            }
+            else
+            {
+                Console.WriteLine("String is not palendrome");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.StringReverse();
+            Console.ReadLine();
+        }
+    }
+}
