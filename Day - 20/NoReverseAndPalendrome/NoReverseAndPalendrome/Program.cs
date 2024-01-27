bb62e28cbfc5cb5871c4141ade8d3bc2e2d3f3e7
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-27 22:50:27 +0530

    Add files via upload
    here we learn and understand how do we make reverse no and check number is palendrome or

---
+++ Day - 20/NoReverseAndPalendrome/NoReverseAndPalendrome/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoReverseAndPalendrome
+{
+    internal class Program
+    {
+     public void CheckPalindrome()
+        {
+            Console.WriteLine("Enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int temp = input;
+            int output = 0;
+
+            while (input != 0)
+            {
+                int rem = input % 10;
+                output = output * 10 + rem;
+                input = input / 10;
+            }
+
+            if (temp == output)
+            {
+                Console.WriteLine(temp + " is a palindrome number.");
+            }
+            else
+            {
+                Console.WriteLine(temp + " is not a palindrome number.");
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.CheckPalindrome();
+            Console.ReadLine();
+        }
+    }
+}
