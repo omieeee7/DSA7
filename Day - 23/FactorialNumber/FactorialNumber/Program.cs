443ddb354c525e91f30b409d6b02cb3ba61ce248
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-30 21:08:05 +0530

    Add files via upload
    Here We learn how to find factorial number of given user define number

---
+++ Day - 23/FactorialNumber/FactorialNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNumber
+{
+    internal class Program
+    {
+        public void FindFactorial()
+        {
+            Console.WriteLine("Enter Number To Find Factorial Of It :-");
+            int input=Convert.ToInt32(Console.ReadLine());
+            int Output = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                 Output = Output * i;
+            }
+            Console.WriteLine("your Factorial Number Is :- "+Output);
+
+        }
+        static void Main(string[] args)
+        {
+            Program p =new Program();
+            p.FindFactorial();
+            Console.ReadLine();
+        }
+    }
+}
