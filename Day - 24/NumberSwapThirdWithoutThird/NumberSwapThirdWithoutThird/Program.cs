37ab189b52ae1330e58843190d222418fd45a682
Omkar Pawar <143325942+omieeee7@users.noreply.github.com>
2024-01-31 22:22:26 +0530

    Add files via upload
    Number Swap With And Without Third Variable

---
+++ Day - 24/NumberSwapThirdWithoutThird/NumberSwapThirdWithoutThird/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSwapThirdWithoutThird
+{
+    internal class Program
+    {
+        private void NumberSwapWithThird()
+        {
+            int NumberOne = 10;
+            int NumberTwo=20;
+            int Swapper;
+            Console.WriteLine("Number Before swapping Number One "+NumberOne+" Number Two "+NumberTwo);
+            Swapper = NumberOne;
+            NumberOne = NumberTwo;
+            NumberTwo = Swapper;
+            Console.WriteLine("Number After swapping Number One " + NumberOne + " Number Two " + NumberTwo);
+        }
+
+        private void NumberSwapWithoutThird()
+        {
+            int NumberOne = 13;
+            int NumberTwo = 20;
+            Console.WriteLine("Number Before swapping Number One " + NumberOne + " Number Two " + NumberTwo);
+            NumberOne = NumberOne + NumberTwo;
+            NumberTwo = NumberOne - NumberTwo;
+            NumberOne= NumberOne - NumberTwo;
+            Console.WriteLine("Number After swapping Number One = {0} , Number Two = {1}",NumberOne,NumberTwo);
+
+        }
+        static void Main(string[] args)
+        {
+            Program p =new Program();
+            //p.NumberSwapWithThird();
+            p.NumberSwapWithoutThird();
+            Console.ReadLine();
+        }
+    }
+}
